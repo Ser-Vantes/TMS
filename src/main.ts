bf587f2d6cf4e62e97de686a7a45c0bc257e85d6
Deviars <92267659+Deviars-Solutions@users.noreply.github.com>
2022-07-12 16:31:18 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,29 @@
-import {NestFactory} from "@nestjs/core";
-import {AppModule} from "./app.module";
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import {JwtAuthGuard} from "./auth/jwt-auth.guard";
-import {ValidationPipe} from "./pipes/validation.pipe";
-
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { JwtAuthGuard } from "./auth/jwt-auth.guard";
+import { ValidationPipe } from "./pipes/validation.pipe";
 
 async function start() {
-    const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule,{ cors: true })
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
+  const config = new DocumentBuilder()
+    .setTitle("Emergo API")
+    .setDescription(" REST API Documentation ")
+    .setVersion("1.0.0")
+    .addTag("Nest JS ")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/api/docs", app, document);
 
-    const config = new DocumentBuilder()
-        .setTitle('Emergo API')
-        .setDescription(' REST API Documentation ')
-        .setVersion('1.0.0')
-        .addTag('Nest JS ')
-        .build()
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('/api/docs', app, document)
+  app.useGlobalPipes(new ValidationPipe());
 
-    app.useGlobalPipes(new ValidationPipe())
-
-    await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 
-start()
+start();
